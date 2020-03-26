91e4c6e39ed7a906ab22701f25c6a0cad81ae12f
Chrystian Lozano <chrystianlozano@icloud.com>
2020-03-26 12:54:36 -0600

    obteniendo datos de la api y pasarla al formulario :D

--- src/context/CategoriasContext.js
+++ src/context/CategoriasContext.js
@@ -1,4 +1,5 @@
 import React, {createContext, useState} from 'react';
+import axios from 'axios'
 
 //Crear el context
 export const CategoriasContext = createContext()
@@ -8,13 +9,24 @@ const CategoriasProvider = (props) => {
 
 
     // crear el state del Context
-    const [hola, guardarHola] = useState('hola')
+    const [categorias, guardarCategorias] = useState([])
 
+    //ejecutar api con axios
+    useState(() => {
+        const obtenerCategorias = async () => {
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`
+
+            const categorias = await axios.get(url)
+
+            guardarCategorias(categorias.data.drinks)
+        }
+        obtenerCategorias()
+    }, [])
 
     return(
         <CategoriasContext.Provider
             value={{
-                hola
+                categorias
             }}
         >
             {props.children}
